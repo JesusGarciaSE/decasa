15c9ce4621068d2b5e90aaa9907645909a71dc1d
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2025-05-25 19:26:54 -0500

    Adds tabbed menu view
    Implements a menu view with tabbed categories
    to allow users to easily browse different food items.
    
    The menu displays food lists based on selected categories.

--- src/views/MenuView.tsx
+++ src/views/MenuView.tsx
@@ -1,12 +1,47 @@
+import { useState } from "react";
 import FoodList from "../components/FoodList";
 import { MENU_DATA } from "../constants/MenuData";
 
 const MenuView = () => {
+  const routes = [
+    { key: "pizza", label: "Pizza" },
+    { key: "extra", label: "Not Pizza" },
+    { key: "other", label: "Toppings" },
+  ];
+  const [activeTab, setActiveTab] = useState("pizza");
+
+  const changeTab = (key: string) => {
+    setActiveTab(key);
+  };
+
   return (
-    <div className="flex flex-col">
-      <FoodList title="Pizza" className="px-10 py-4" foodList={MENU_DATA.pizzas} />
+    <div className='h-auto w-full items-center flex flex-col gap-4'>
+      <div className='flex gap-4'>
+        {routes.map((route) => (
+          <div
+            key={route.key}
+            className='p-3 text-decasa font-decasa text-lg font-semibold cursor-pointer'
+            onClick={() => changeTab(route.key)}
+          >
+            {route.label}
+          </div>
+        ))}
+      </div>
+      <div className='flex flex-col'>
+        <FoodList
+          title={routes.find((route) => route.key === activeTab)!.label}
+          className={"px-10 py-4"}
+          foodList={
+            activeTab === "pizza"
+              ? MENU_DATA.pizzas
+              : activeTab === "extra"
+              ? MENU_DATA.others
+              : MENU_DATA.toppings
+          }
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default MenuView;
