97ee6605a15b0a668ce2b075cf2cdfdae6453144
Jesus Garcia <Jesus.R.Garcia9191@gmail.com>
2025-05-25 19:26:44 -0500

    Adjusts z-index for background images.
    Updates z-index of background images to ensure proper layering and visibility of other page elements.

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -5,12 +5,12 @@ import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <div className='flex flex-col w-screen min-h-screen items-center justify-center overflow-x-scroll'>
+    <div className='relative flex flex-col h-screen min-h-screen items-center justify-center overflow-x-scroll'>
       <img
         src={decasaman}
         alt='Background'
         className={
-          "h-auto max-w-full absolute inset-0 object-cover z-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-25"
+          "h-auto w-auto absolute -z-10 inset-0 object-cover top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-25"
         }
       />
       <Header />
--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -6,7 +6,7 @@ const BackgroundImage: React.FC<ICustomizableComponent> = ({ className }) => {
     <img
       src={decasaman}
       alt="Background"
-      className={className + " h-auto max-w-full absolute inset-0 object-cover z-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"}
+      className={className + " h-auto max-w-full absolute inset-0 object-cover -z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"}
     />
   );
 };
